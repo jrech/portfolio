e69163475a214a664a82957393b4616c7d9bc1e7
jrech <jonasrech@gmail.com>
2026-08-11 16:03:58 +0200

    Add hosting-compatible server build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://www.jonasrech.com',
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
   integrations: [sitemap()],
   i18n: {
     defaultLocale: 'en',
