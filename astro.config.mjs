a877e6a45c79f53d6b5f6eaf12b219c2933962fb
jrech <jonasrech@gmail.com>
2026-08-11 16:14:43 +0200

    Restore static Astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://www.jonasrech.com',
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
-  vite: {
-    ssr: {
-      noExternal: true,
-    },
-  },
   integrations: [sitemap()],
   i18n: {
     defaultLocale: 'en',
